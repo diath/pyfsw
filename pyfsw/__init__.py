4d92a06f4ee2baaf3af09b3bab3935a17f4900a3
Kamil Chojnowski <diath@zodiaclabs.org>
2014-06-22 01:33:50 +0200

    Add custom error pages (403, 404, 500)

--- pyfsw/__init__.py
+++ pyfsw/__init__.py
@@ -43,6 +43,21 @@ def filter_vocation(value):
 def filter_town(value):
 	return TOWNS[value]
 
+@app.errorhandler(403)
+def error_forbidden(error):
+	return render_template('error.htm', error=403,
+		message='Access to the resource you are trying to reach is forbidden.'), 403
+
+@app.errorhandler(404)
+def error_not_found(error):
+	return render_template('error.htm', error=404,
+		message='The page you are trying to reach has not been found.'), 404
+
+@app.errorhandler(500)
+def error_internal(error):
+	return render_template('error.htm', error=500,
+		message='Internal server error, please contact the administrator.'), 500
+
 def login_required(f):
 	@wraps(f)
 	def decorated(*args, **kwargs):
