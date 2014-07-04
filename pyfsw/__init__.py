a1b4f2a81f3950a39fccefda6fd4b4c3fb075755
Kamil Chojnowski <diath@zodiaclabs.org>
2014-07-04 17:24:03 +0200

    Move template filters to their own file

--- pyfsw/__init__.py
+++ pyfsw/__init__.py
@@ -23,42 +23,6 @@ if DEBUG:
 
 db = SQLAlchemy(app)
 
-@app.template_filter('datetime')
-def filter_datetime(value):
-	return datetime.fromtimestamp(int(value)).strftime(DATE_FORMAT)
-
-@app.template_filter('timetotal')
-def filter_timetotal(value):
-	value = int(value)
-
-	hour = value / 3600
-	rem = value % 3600
-	minute = rem / 60
-	second = rem % 60
-
-	return '{:02d} hours, {:02d} minutes and {:02d} seconds'.format(int(hour), int(minute), int(second))
-
-@app.template_filter('gender')
-def filter_gender(value):
-	return GENDERS[value]
-
-@app.template_filter('vocation')
-def filter_vocation(value):
-	return VOCATIONS[value]
-
-@app.template_filter('town')
-def filter_town(value):
-	return TOWNS[value]
-
-@app.template_filter('rank')
-def filter_rank(value):
-	if value == 3:
-		return 'The Leader'
-	elif value == 2:
-		return 'Vice Leader'
-
-	return 'Member'
-
 def login_required(f):
 	@wraps(f)
 	def decorated(*args, **kwargs):
@@ -104,6 +68,8 @@ def is_guild_leader(id):
 	return found
 
 app.jinja_env.globals.update(get_library=get_library)
+
+from pyfsw.filters import *
 
 from pyfsw.models.account import Account
 from pyfsw.models.guild import Guild, GuildInvite, GuildMembership, GuildRank, GuildWar, GuildWarKill
---
+++ pyfsw/filters.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+from pyfsw import app
+from pyfsw import DATE_FORMAT, GENDERS, VOCATIONS, TOWNS
+
+@app.template_filter('datetime')
+def filter_datetime(value):
+	return datetime.fromtimestamp(int(value)).strftime(DATE_FORMAT)
+
+@app.template_filter('timetotal')
+def filter_timetotal(value):
+	value = int(value)
+
+	hour = value / 3600
+	rem = value % 3600
+	minute = rem / 60
+	second = rem % 60
+
+	return '{:02d} hours, {:02d} minutes and {:02d} seconds'.format(int(hour), int(minute), int(second))
+
+@app.template_filter('gender')
+def filter_gender(value):
+	return GENDERS[value]
+
+@app.template_filter('vocation')
+def filter_vocation(value):
+	return VOCATIONS[value]
+
+@app.template_filter('town')
+def filter_town(value):
+	return TOWNS[value]
+
+@app.template_filter('rank')
+def filter_rank(value):
+	if value == 3:
+		return 'The Leader'
+	elif value == 2:
+		return 'Vice Leader'
+
+	return 'Member'
