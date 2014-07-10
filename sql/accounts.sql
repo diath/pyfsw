ef7ace8de9cb2e597eb0bdebf33437e21afdcf99
Kamil Chojnowski <diath@zodiaclabs.org>
2014-07-11 00:26:16 +0200

    Add forum schema

--- sql/accounts.sql
+++ sql/accounts.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `accounts` ADD `key` VARCHAR(19) AFTER `creation`;
 ALTER TABLE `accounts` ADD `points` INT(8) AFTER `key`;
+ALTER TABLE `accounts` ADD `lastpost` INT(11) AFTER `points`;
---
+++ sql/forum.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS `forum_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `forum_boards` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL,
+  `description` text,
+  `category_id` int(11) DEFAULT NULL,
+  `locked` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `forum_threads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `subject` varchar(64) DEFAULT NULL,
+  `timestamp` int(11) DEFAULT NULL,
+  `board_id` int(11) DEFAULT NULL,
+  `locked` int(11) DEFAULT NULL,
+  `pinned` int(11) DEFAULT NULL,
+  `lastpost` int(11) DEFAULT NULL,
+  `author_id` int(11) DEFAULT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `board_id` (`board_id`),
+  KEY `author_id` (`author_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `forum_posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author_id` int(11) DEFAULT NULL,
+  `content` text,
+  `timestamp` int(11) DEFAULT NULL,
+  `thread_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `author_id` (`author_id`),
+  KEY `thread_id` (`thread_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
