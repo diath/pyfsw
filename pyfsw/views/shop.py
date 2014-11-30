9a48f7274bae46ae91bdb5bc5a5e119550eecad2
Kamil Chojnowski <diath@zodiaclabs.org>
2014-11-30 22:41:52 +0100

    Display premium points on the shop offer page

--- pyfsw/views/shop.py
+++ pyfsw/views/shop.py
@@ -8,7 +8,12 @@ from pyfsw import login_required, current_user
 @app.route('/shop/offer')
 def route_shop():
 	categories = db.session.query(ShopCategory).all()
-	return render_template('shop/browse.htm', categories=categories)
+	user = current_user()
+
+	return render_template(
+		'shop/browse.htm',
+		categories=categories, user=user
+	)
 
 @app.route('/shop/order/<int:id>', methods=['GET'])
 @login_required
